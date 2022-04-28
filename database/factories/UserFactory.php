6614683688a5ae51983260dad29bc951ed19a36c
sunak27 <alfadsabilhaq@gmail.com>
2022-04-29 06:01:02 +0700

    Seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -16,9 +16,11 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name(),
+            'username' => $this->faker->userName(),
             'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
+            'email_verified_at' => null,
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'role_id' => $this->faker->numberBetween(1, 3),
             'remember_token' => Str::random(10),
         ];
     }
